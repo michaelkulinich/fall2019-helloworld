02ea38a90a37278323e25a65ee6892ab08135df1
Michael Kulinich <kulinich@chapman.edu>
2019-09-04 02:48:17 +0000

    final helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,7 @@
+#include <iostream> //instructs preprocessor to include iostream
+using namespace std; //standard library
+
+int main() {
+  cout << "Hello World\n" ; //outputs Hello World into the console
+  return 0;
+}
